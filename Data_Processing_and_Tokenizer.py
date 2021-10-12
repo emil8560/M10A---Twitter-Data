acf57418f6128420aa99fd3f7dae1e05a32fed40
emil8560 <ebm@hotmail.dk>
2021-10-12 09:04:14 +0200

    Function Based Tree Created

--- Data_Processing_and_Tokenizer.py
+++ Data_Processing_and_Tokenizer.py
@@ -11,7 +11,29 @@ Retweets = []
 Comments = []
 
 def tokenizer_spacy(text):  # Definerer funktion ud fra koden fra tidligere
-    custom_stops = ["god", "al", "stor", "ny", "tak", "dag"]  # Definerer kontekstspecifikke stopord
+    custom_stops = ["ad", "af", "aldrig", "alene", "alle", "allerede", "alligevel", "alt", "altid", "anden", "andet",
+                    "andre", "at", "bag", "bare", "begge", "bl.a.", "blandt", "blev", "blive", "bliver", "burde",
+                    "bør", "ca.", "da", "de", "dem", "den", "denne", "dens", "der", "derefter", "deres", "derfor",
+                    "derfra", "deri", "dermed", "derpå", "derved", "det", "dette", "dig", "din", "dine", "disse",
+                    "dit", "dog", "du", "efter", "egen", "ej", "eller", "ellers", "en", "end", "endnu", "ene",
+                    "eneste", "enhver", "ens", "enten", "er", "et", "f.eks.", "far", "fem", "fik", "fire", "flere",
+                    "flest", "fleste", "for", "foran", "fordi", "forrige", "fra", "fx", "få", "får", "før", "først",
+                    "gennem", "gjorde", "gjort", "god", "godt", "gør", "gøre", "gørende", "ham", "han", "hans", "har",
+                    "havde", "have", "hej", "hel", "heller", "helt", "hen", "hende", "hendes", "henover", "her",
+                    "herefter", "heri", "hermed", "herpå", "hos", "hun", "hvad", "hvem", "hver", "hvilke", "hvilken",
+                    "hvilkes", "hvis", "hvor", "hvordan", "hvorefter", "hvorfor", "hvorfra", "hvorhen", "hvori",
+                    "hvorimod", "hvornår", "hvorved", "i", "igen", "igennem", "ikke", "imellem", "imens", "imod",
+                    "ind", "indtil", "ingen", "intet", "ja", "jeg", "jer", "jeres", "jo", "kan", "kom", "komme",
+                    "kommer", "kun", "kunne", "lad", "langs", "lav", "lave", "lavet", "lidt", "lige", "ligesom",
+                    "lille", "længere", "man", "mand", "mange", "med", "meget", "mellem", "men", "mens", "mere",
+                    "mest", "mig", "min", "mindre", "mindst", "mine", "mit", "mod", "må", "måske", "ned", "nej",
+                    "nemlig", "ni", "nogen", "nogensinde", "noget", "nogle", "nok", "nu", "ny", "nyt", "når", "nær",
+                    "næste", "næsten", "og", "også", "okay", "om", "omkring", "op", "os", "otte", "over", "overalt",
+                    "pga.", "på", "samme", "sammen", "se", "seks", "selv", "selvom", "senere", "ser", "ses", "siden",
+                    "sig", "sige", "sin", "sine", "sit", "skal", "skulle", "som", "stadig", "stor", "store", "synes",
+                    "syntes", "syv", "så", "sådan", "således", "tag", "tage", "temmelig", "thi", "ti", "tidligere",
+                    "til", "tilbage", "tit", "to", "tre", "ud", "uden", "udover", "under", "undtagen", "var", "ved",
+                    "vi", "via", "vil", "ville", "vor", "vore", "vores", "vær", "være", "været", "øvrigt"]  
     default_stopwords = list(nlp.Defaults.stop_words)  # Indlæser prædefineret stopordsliste
     stop_words = default_stopwords + custom_stops  # Danner samlet stopordsliste
     pos_tags = ['PROPN', 'ADJ', 'NOUN']  # Definerer POS-tags som skal bevares: egenavne, adjektiver og navneord
@@ -37,60 +59,76 @@ def convert_to_list(type, list, data): # Funktion der tilføjer elementer fra da
     for x in range(len(data)):
         list.append(data.loc[x, type])
 
+def post_to_df(data: list, df, ):
+    stop_count = 0
+    stop_value = 6000
+
+    for posts in range(len(data)):
+        opdelt_tekst = tokenizer_spacy(data.loc[posts, 'full_text'])
+        antal_likes = Likes[posts]
+        antal_retweets = Retweets[posts]
+        # antal_comments = Comments[posts]
+        print(posts, "ud af ", len(data), "DataProcessing")
+        stop_count = stop_count + 1
+        if stop_count > stop_value:
+            break
+        for index_words in range(len(opdelt_tekst)):
+            ord = opdelt_tekst[index_words]
+            if df.loc[df['word'] == ord].empty == True:
+                lenght = len(df) + 1
+                df.loc[lenght, 'word'] = ord
+                df.loc[lenght, 'likes'] = antal_likes
+                df.loc[lenght, 'retweets'] = antal_retweets
+                # df.loc[lenght, 'comments'] = antal_comments
+                df.loc[lenght, 'n'] = 1
+            else:
+                row = df.index[df['word'] == ord]
+                add_likes = df.loc[row, 'likes'] + antal_likes
+                df.loc[row, 'likes'] = add_likes
+                add_retweets = df.loc[row, 'retweets'] + antal_retweets
+                df.loc[row, 'retweets'] = add_retweets
+                # add_comments = df.loc[row, 'comments'] + antal_comments
+                # df.loc[row, 'comments'] = add_comments
+                add_n = df.loc[row, 'n'] + 1
+                df.loc[row, 'n'] = add_n
+
+def create_dataframe(name: str):
+    d = {'word': [], 'likes': [], 'retweets': [], 'comments': [], 'n': [],
+         'avg_likes': [], 'avg_retweets': [], 'avg_comments': [], 'trafic': [], 'party': []}
+    name = pd.DataFrame(data=d)
+    return name
+
+def calc_average(df):
+    for n in range(len(df)):
+        print(n, "ud af", len(df), "Calc Avg")
+        row = df.index[n]
+        avg_likes = df.loc[row, 'likes'] / df.loc[row, 'n']
+        df.loc[row, 'avg_likes'] = avg_likes
+        avg_retweets = df.loc[row, 'retweets'] / df.loc[row, 'n']
+        df.loc[row, 'avg_retweets'] = avg_retweets
+        # avg_comments = df.loc[row, 'comments'] / df.loc[row, 'n']
+        # df.loc[row, 'avg_comments'] = avg_comments
+        trafic = df.loc[row, 'likes'] + df.loc[row, 'retweets']  # + df.loc[row, 'comments']
+        df.loc[row, 'trafic'] = trafic
+
+def df_to_csv(df, name: str):
+    df.to_csv(name, float_format='%.0f')
+
+
+
+# ---------- RUN TIME Main ------------
 convert_to_list('retweet_count',Retweets, tweetdata)
 convert_to_list('favorite_count',Likes, tweetdata)
 
-d = {'word':[], 'likes':[],'retweets':[],'comments':[],'n':[],
-     'avg_likes':[],'avg_retweets':[],'avg_comments':[],'trafic':[]}
-df = pd.DataFrame(data=d)
-df
-stop_count = 0
-stop_value = 6000
-
-for posts in range(len(tweetdata)):
-    opdelt_tekst = tokenizer_spacy(tweetdata.loc[posts, 'full_text'])
-    antal_likes = Likes[posts]
-    antal_retweets = Retweets[posts]
-    #antal_comments = Comments[posts]
-    print(posts, "ud af ", len(tweetdata), "DataProcessing")
-    stop_count = stop_count + 1
-    if stop_count > stop_value:
-        break
-    for index_words in range(len(opdelt_tekst)):
-        ord = opdelt_tekst[index_words]
-        if df.loc[df['word'] == ord].empty == True:
-            lenght = len(df) + 1
-            df.loc[lenght, 'word'] = ord
-            df.loc[lenght, 'likes'] = antal_likes
-            df.loc[lenght, 'retweets'] = antal_retweets
-            #df.loc[lenght, 'comments'] = antal_comments
-            df.loc[lenght, 'n'] = 1
-        else:
-            row = df.index[df['word'] == ord]
-            add_likes = df.loc[row, 'likes'] + antal_likes
-            df.loc[row, 'likes'] = add_likes
-            add_retweets = df.loc[row, 'retweets'] + antal_retweets
-            df.loc[row, 'retweets'] = add_retweets
-            #add_comments = df.loc[row, 'comments'] + antal_comments
-            #df.loc[row, 'comments'] = add_comments
-            add_n = df.loc[row, 'n'] + 1
-            df.loc[row, 'n'] = add_n
-
-for n in range(len(df)):
-    print(n, "ud af", len(df), "Calc Avg")
-    row = df.index[n]
-    avg_likes = df.loc[row, 'likes'] / df.loc[row, 'n']
-    df.loc[row, 'avg_likes'] = avg_likes
-    avg_retweets = df.loc[row, 'retweets'] / df.loc[row, 'n']
-    df.loc[row, 'avg_retweets'] = avg_retweets
-    #avg_comments = df.loc[row, 'comments'] / df.loc[row, 'n']
-    #df.loc[row, 'avg_comments'] = avg_comments
-    trafic = df.loc[row, 'likes'] + df.loc[row, 'retweets'] # + df.loc[row, 'comments']
-    df.loc[row, 'trafic'] = trafic
 
 
 
-df.to_csv('midlertidigt_data.csv', float_format='%.0f')
+
+
+
+
+
+
 
 
 # print(Retweets)
